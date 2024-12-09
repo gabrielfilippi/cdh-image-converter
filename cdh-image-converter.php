b2bd7753c9dac04383c955448179e6729dce68c9
gabrielfilippi <gabriel._gf@hotmail.com>
2024-12-09 09:40:42 -0300

    fix issues and improve performance

--- cdh-image-converter.php
+++ cdh-image-converter.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: CodeHive Image Converter to WebP
 Description: The best image converter plugin on the market, simple, lightweight and optimized. Convert your images to WebP, force them to load and have a much faster website.
-Version: 1.2.0
-Author: CodeHive 
+Version: 2.0.0
+Author: CodeHive
 Author URI: https://codehive.com.br
 License: GPL-2.0+
 License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -43,33 +43,45 @@ add_action( 'before_woocommerce_init', function() {
  * 
  */
 function cdh_image_converter_activation() {
-    
+
     if (check_image_libraries()) {
         global $wpdb;
 
         $table_name = CDH_TABLE_WEBP_CONVERSION;
         $charset_collate = $wpdb->get_charset_collate();
-    
+
         $sql = "CREATE TABLE IF NOT EXISTS $table_name (
             id INT(11) NOT NULL AUTO_INCREMENT,
             attachment_id INT(11) NOT NULL,
             image_path VARCHAR(255) NOT NULL,
+            status ENUM('pending', 'completed', 'error') DEFAULT 'pending',
             size VARCHAR(50) NULL,
-            PRIMARY KEY (id)
+            PRIMARY KEY (id),
+            INDEX status_index (status) -- Índice para o campo ENUM status
         ) $charset_collate;";
-    
+
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
-    
-        if (!wp_next_scheduled('cdh_convert_images_to_webp_cron_event')) {
-            wp_schedule_event(time(), 'cdh_img_converter_daily', 'cdh_convert_images_to_webp_cron_event');
+
+        // Verificar se a coluna já existe
+        $column_exists = $wpdb->get_results("SHOW COLUMNS FROM `$table_name` LIKE 'status'");
+        if (empty($column_exists)) {
+            $wpdb->query("ALTER TABLE $table_name ADD COLUMN status ENUM('pending', 'completed', 'error') NOT NULL DEFAULT 'pending'");
+            $wpdb->query("ALTER TABLE $table_name ADD INDEX status_index (status)");
         }
 
+        // add cron schedule
+        cdh_active_scheduled_to_convert_images();
     }
 
 }
 register_activation_hook(__FILE__, 'cdh_image_converter_activation');
 
+function cdh_active_scheduled_to_convert_images(){
+    if (!wp_next_scheduled('cdh_convert_images_to_webp_cron_event')) {
+        wp_schedule_event(time(), 'cdh_img_converter_6hourly', 'cdh_convert_images_to_webp_cron_event');
+    }
+}
 
 /**
  * We clear crons on disabled plugin
@@ -80,9 +92,9 @@ function cdh_image_converter_deactivation() { // runs on plugin is disabled;
 }
 
 /**
- * 
+ *
  * When plugin is loaded, verify if plugin can be work
- * 
+ *
  */
 add_action('plugins_loaded', function() {
     if (check_image_libraries()) {
@@ -202,9 +214,9 @@ function convert_thumbnails_to_webp($post_id) {
 }
 
 /**
- * 
+ *
  * Remove all Webp Imagens when exclude Originals images
- * 
+ *
  */
 function delete_webp_images($post_id) {
     $thumbnail_sizes = get_intermediate_image_sizes();
@@ -244,7 +256,7 @@ function convert_images_to_webp_on_upload($metadata, $attachment_id) {
             update_post_meta($attachment_id, '_wp_attachment_webp', $webp_image);
         }
 
-        convert_thumbnails_to_webp($attachment_id);
+        //convert_thumbnails_to_webp($attachment_id);
     }
 
     return $metadata;
@@ -280,10 +292,10 @@ function force_webp_image($image, $attachment_id, $size) {
 
                 // Remove a parte do caminho que corresponde ao diretório de uploads
                 $path = str_replace($upload_dir['basedir'], '', $image_path);
-            
+
                 // Remove a barra inicial, se houver
                 $path = ltrim($path, '/');
-            
+
                 // Constrói a URL completa do arquivo
                 $new_image_url = $base_url . '/' . $path;
 
@@ -297,10 +309,10 @@ function force_webp_image($image, $attachment_id, $size) {
                         'image_path' => $image_path,
                         'size' => $size
                     );
-        
+
                     add_attachament_to_database($data);
                 }
-                
+
             }
         }
     }
@@ -319,17 +331,17 @@ function force_webp_implements_in_html( $html, $attachment_id, $size, $icon, $at
         if(isset($attr['srcset'])){
             $srcset = $attr['srcset'];
         }
-    
+
         $sizes = "";
         if(isset($attr['sizes'])){
             $sizes = $attr['sizes'];
         }
-    
+
         $filenameWithoutExt = substr($originalSource, 0, strrpos($originalSource, "."));
-    
+
         $webpOriginaFilePlusWebpSrc = WP_CONTENT_DIR . "/" . strstr($originalSource, "uploads") . ".webp";
         $webpOriginaFileMinusExtSrc = WP_CONTENT_DIR . "/" . strstr($filenameWithoutExt, "uploads") . ".webp";
-    
+
         $hasPicture = false;
         $blogUrl = get_bloginfo('wpurl');
         if(file_exists($webpOriginaFilePlusWebpSrc)){
@@ -337,14 +349,14 @@ function force_webp_implements_in_html( $html, $attachment_id, $size, $icon, $at
             $srcset = str_replace($originalSource, $newSource, $srcset);
             $html = str_replace($originalSource, $newSource, $html);
             $hasPicture = true;
-    
+
         }else if(file_exists($webpOriginaFileMinusExtSrc)){
             $newSource = $blogUrl . "/wp-content/" . strstr($webpOriginaFileMinusExtSrc, "uploads");
             $srcset = str_replace($originalSource, $newSource, $srcset);
             $html = str_replace($originalSource, $newSource, $html);
             $hasPicture = true;
         }
-    
+
         if($hasPicture){
             $html = '<picture><source type="image/webp" srcset="'.$srcset.'" sizes="'.$sizes.'">'.$html.'</picture>';
         }else{
@@ -364,20 +376,20 @@ function force_webp_implements_in_html( $html, $attachment_id, $size, $icon, $at
             $file_extension = pathinfo($image_path, PATHINFO_EXTENSION);
 
             $image_path = $file_dirname . '/' . $file_name_no_ext . '.' . $file_extension;
-            
+
             if(is_file($image_path)){
                 $data = array(
                     'attachment_id' => $attachment_id,
                     'image_path' => $image_path,
                     'size' => $size
                 );
-    
+
                 add_attachament_to_database($data);
             }
-            
+
         }
     }
-    
+
     return $html;
 }
 
@@ -399,9 +411,9 @@ function set_webp_quality($quality) {
 add_filter('wp_editor_set_quality', 'set_webp_quality', 99999);
 
 /**
- * 
+ *
  * Save attachament in database to convert in webp images
- * 
+ *
  */
 function add_attachament_to_database($data){
     global $wpdb;
@@ -433,72 +445,86 @@ function add_attachament_to_database($data){
 }
 
 /**
- * 
+ *
  * Convert images in background
- * 
+ *
  */
 add_action( 'cdh_convert_images_to_webp_cron_event', 'cdh_convert_images_to_webp_in_background');
 function cdh_convert_images_to_webp_in_background() {
-    global $wpdb;
-    $table_name = CDH_TABLE_WEBP_CONVERSION;
-
-    // Obtém os dados de conversão pendentes da tabela
-    $conversion_data = $wpdb->get_results("SELECT * FROM $table_name LIMIT 30");
+    $conversion_data = cdh_get_pending_conversions();
 
     if ($conversion_data) {
         foreach ($conversion_data as $data) {
+            global $wpdb;
+            $table_name = CDH_TABLE_WEBP_CONVERSION;
+
+            // Atualizar o status para 'in_progress'
+            $wpdb->update(
+                $table_name,
+                array('status' => 'in_progress'),
+                array('id' => $data->id),
+                array('%s'),
+                array('%d')
+            );
+
             $image_path = $data->image_path;
+            $webp_image = convert_to_webp($image_path);
 
-            $file_dirname = pathinfo( $image_path, PATHINFO_DIRNAME );
-            $file_name_no_ext = pathinfo( $image_path, PATHINFO_FILENAME );
-
-            $webp_image = $file_dirname . '/' . $file_name_no_ext . '.webp';
-            // Verifica novamente se o arquivo em formato WebP ainda não existe
-            if (!file_exists($webp_image)) {
-                $webp_return = convert_to_webp($image_path);
-
-                if ($webp_return) {
-                    if($data->size){
-                        update_post_meta($data->attachment_id, '_wp_attachment_' . $data->size . '_webp', $webp_return);
-                    }else{
-                        update_post_meta($data->attachment_id, '_wp_attachment_webp', $webp_return);
-                    }
-                    // Remove a entrada da tabela após a conversão bem-sucedida
-                    $wpdb->delete(
-                        $table_name,
-                        array(
-                            'id' => $data->id
-                        ),
-                        array(
-                            '%d'
-                        )
-                    );
+            if ($webp_image) {
+                if ($data->size) {
+                    update_post_meta($data->attachment_id, '_wp_attachment_' . $data->size . '_webp', $webp_image);
+                } else {
+                    update_post_meta($data->attachment_id, '_wp_attachment_webp', $webp_image);
                 }
-            } else {
-                // Remove a entrada da tabela se o arquivo em formato WebP já existir
-                $wpdb->delete(
+
+                // Atualizar o status para 'completed'
+                $wpdb->update(
                     $table_name,
-                    array(
-                        'id' => $data->id
-                    ),
-                    array(
-                        '%d'
-                    )
+                    array('status' => 'completed'),
+                    array('id' => $data->id),
+                    array('%s'),
+                    array('%d')
+                );
+            } else {
+                // Atualizar o status para 'error'
+                $wpdb->update(
+                    $table_name,
+                    array('status' => 'error'),
+                    array('id' => $data->id),
+                    array('%s'),
+                    array('%d')
                 );
             }
         }
     }
+
+    // add cron schedule
+    cdh_active_scheduled_to_convert_images();
 }
 
 /**
  * We configure custom runtimes for the crons we create
  */
-add_filter( 'cron_schedules',  'cdh_img_converter_custom_cron_schedule' );
-function cdh_img_converter_custom_cron_schedule( $schedules ) {
-    $schedules['cdh_img_converter_daily'] = array(
-        'interval' => 14400, // Every 14400 seconds = 4 hours
-        'display'  => __( 'Every 4 hours' ),
-    );
-
+add_filter('cron_schedules', function($schedules) {
+    $schedules['cdh_img_converter_6hourly'] = [
+        'interval' => 21600, // A cada 6 horas
+        'display' => __('A cada 6 horas')
+    ];
     return $schedules;
+});
+
+/**
+ * Get pending images to convert to webp
+ */
+function cdh_get_pending_conversions($limit = 30) {
+    global $wpdb;
+    $table_name = CDH_TABLE_WEBP_CONVERSION;
+
+    return $wpdb->get_results(
+        $wpdb->prepare(
+            "SELECT * FROM $table_name WHERE status = %s LIMIT %d",
+            'pending',
+            $limit
+        )
+    );
 }
